984eda16a05dae6f9571b41ab5ed77b568086557
Hassan Ogunniyi <ogunniyi95@gmail.com>
2021-12-09 16:40:00 +0100

    Second commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,8 +1,10 @@
 
+let user = 'octocat'
+
 async function getUsers(){
-    const res = await fetch('https://api.github.com/users/cloudmate404')
+    const res = await fetch(`https://api.github.com/users/${user}`)
     const data = await res.json()
         console.log(data)
 }
 
-getUsers()
+getUsers(user)
