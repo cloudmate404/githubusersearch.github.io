02c385a8a25843dda67baf8cbce6ebadf57bfe17
Hassan Ogunniyi <ogunniyi95@gmail.com>
2021-12-13 17:51:39 +0100

    code fixed, might be last commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,47 +1,43 @@
+const theme = document.getElementById('theme')
 const form = document.getElementById('form')
 const searchInput = document.getElementById('searchInput')
-const theme = document.getElementById('theme')
+const errorMessage = document.getElementById('errorMessage')
+const button = document.getElementById('button')
 const userCard =document.getElementById('userCard')
+const userImage = document.getElementById('userImage')
+const nameDisplay = document.getElementById('name')
+const login = document.getElementById('login')
+const dateCreated = document.getElementById('date')
+const userBio = document.getElementById('userBio')
+const repos = document.getElementById('repos')
+const followers = document.getElementById('followers')
+const following = document.getElementById('following')
+const locationDisplay = document.getElementById('location')
+const twitter = document.getElementById('twitter')
+const blogLink = document.getElementById('blog')
+const companyName = document.getElementById('company')
 
 let user 
 
-// async function getUserData(user){
-//   let res = await fetch(`https://api.github.com/users/${user}`)
-//                       if(res.status == 200){
-//                         document.getElementById('errorMessage').textContent = ''
-//                         let data = await res.json()
-//                         console.log(data)
-//                         // return data
-//                         renderData(data)
-//                       } throw new Error(res.status)
-
-//                       .catch(Error =>{
-//                         document.getElementById('errorMessage').textContent = 'No result'
-//                         document.getElementById('searchInput').placeholder =''
-//                         document.getElementById('searchInput').style.width='25%'
-//                       })
-                      
-//                     }
-               
+              
 
 function getUserData(user){
     fetch(`https://api.github.com/users/${user}`)
         .then(res => {
           if(res.status == 200){
-            document.getElementById('errorMessage').textContent = ''
+            errorMessage.textContent = ''
             return res.json()
           }else{
             throw new Error(res.status)
           }
         })
         .then(data =>{
-            console.log(data)
             renderData(data)
         })
         .catch(Error =>{
-          document.getElementById('errorMessage').textContent = 'No result'
-          document.getElementById('searchInput').placeholder =''
-          document.getElementById('searchInput').style.width='25%'
+          errorMessage.textContent = 'No result'
+          searchInput.placeholder =''
+          searchInput.style.width='25%'
         })
     
 }
@@ -53,57 +49,58 @@ function renderData(data){
       // let login = data.login
 
       if(data.name === null){
-          document.getElementById('name').textContent = data.login
+          nameDisplay.textContent = data.login
       }else{
-         document.getElementById('name').textContent = data.name
+         nameDisplay.textContent = data.name
       }
 
-      document.getElementById('login').textContent =  data.login
-      document.getElementById('userImage').src = data.avatar_url
-      document.getElementById('date').textContent =`Joined ${createdDate}`
+      login.textContent =  data.login
+      userImage.src = data.avatar_url
+      dateCreated.textContent =`Joined ${createdDate}`
 
       if(data.bio === null){
-        document.getElementById('userBio').textContent= 'This profile has no bio.'
+        userBio.textContent= 'This profile has no bio.'
       }else{
-        document.getElementById('userBio').textContent = data.bio
+        userBio.textContent = data.bio
       }
 
-      document.getElementById('repos').textContent = data.public_repos
-      document.getElementById('followers').textContent = data.followers
-      document.getElementById('following').textContent =data.following
+      repos.textContent = data.public_repos
+      followers.textContent = data.followers
+      following.textContent =data.following
       
       if(data.location === null){
-        document.getElementById('location').textContent = 'Not available'
+        locationDisplay.textContent = 'Not available'
         document.querySelector('.location').classList.add('notAvailable')
       }else{
-        document.getElementById('location').textContent = data.location
+        locationDisplay.textContent = data.location
         document.querySelector('.location').classList.remove('notAvailable')
       }
 
       if(data.blog === ''){
-        document.getElementById('blog').textContent = 'Not available'
+        blogLink.textContent = 'Not available'
         document.querySelector('.blog').classList.add('notAvailable')
 
       }else{
-        document.getElementById('blog').textContent = data.blog
+        blogLink.textContent = data.blog
+        blogLink.href = data.blog
         document.querySelector('.blog').classList.remove('notAvailable')
       }
 
       if(data.twitter_username === null){
-        document.getElementById('twitter').textContent = 'Not available'
+        twitter.textContent = 'Not available'
         document.querySelector('.twitter').classList.add('notAvailable')
 
       }else{
-        document.getElementById('twitter').textContent = data.twitter_username
+        twitter.textContent = data.twitter_username
         document.querySelector('.twitter').classList.remove('notAvailable')
       }
 
       if(data.company === null){
-        document.getElementById('company').textContent = 'Not available'
+        companyName.textContent = 'Not available'
         document.querySelector('.company').classList.add('notAvailable')
 
       }else{
-        document.getElementById('company').textContent = data.company
+        companyName.textContent = data.company
         document.querySelector('.company').classList.remove('notAvailable')
       }
 
@@ -113,7 +110,7 @@ function renderData(data){
 
 form.addEventListener('submit',e=>{
     e.preventDefault()
-    console.log(searchInput.value)
+    
     user = searchInput;
     getUserData(searchInput.value)
     searchInput.value =''
@@ -122,8 +119,8 @@ form.addEventListener('submit',e=>{
 
 
 document.getElementById('searchInput').addEventListener('click',()=>{
-  document.getElementById('errorMessage').textContent = ''
-  document.getElementById('searchInput').style.width='100%'
+  errorMessage.textContent = ''
+  searchInput.style.width='100%'
 })
 
 
@@ -136,11 +133,16 @@ theme.addEventListener('click',()=>{
     <p>Light</p>
     <img src="/assets/icon-sun.svg" alt="" class="themeIcon">
     `
+    theme.classList.remove('theme')
+    theme.classList.add('lightTheme')
   }else{
     theme.innerHTML =`
     <p>Dark</p>
     <img src="/assets/icon-moon.svg" alt="" class="themeIcon">
     `
+    theme.classList.remove('lightTheme')
+    theme.classList.add('theme')
+    
   }
   
 
